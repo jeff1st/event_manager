9c5bda78ca36b28c8ea47c05313930b77e46e192
jeff1st <jeff1st@gmail.com>
2017-01-30 21:00:37 +0100

    Creating form_letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,32 @@
-puts "Event manager initialized!"
+require "csv"
+require "sunlight/congress"
 
-lines = File.readlines "event_attendees.csv"
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
 
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  
+  legislator_names = legislators.collect do |leg|
+    "#{leg.first_name} #{leg.last_name}"
+  end
+  
+  return legislator_names.join(", ")
+end
+
+puts "EventManager initialized"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
 
